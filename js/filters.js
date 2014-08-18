931e4a7cafd3df5e430a4595ff842d5b6f97aa04
Phillip <phillip.chen@hotmail.com>
2014-08-17 19:28:29 -0700

    human readable date filter

--- js/filters.js
+++ js/filters.js
@@ -3,4 +3,14 @@ angular.module('jacatucla')
   return function(text) {
     return $sce.trustAsHtml(text);
   };
-}]);
+}])
+.filter('convert_rfc_date', function() {
+  return function(date_string) {
+    var unix_time = new Date(date_string);
+    var months = ["January", "February", "March", "April", "June", "July", "August", "September", "October", "November", "December"];
+    var month = months[unix_time.getMonth() - 1];
+    var day = unix_time.getDate();
+    var year = unix_time.getFullYear();
+    return month + ', ' + day + ' ' + year;
+  };
+});
