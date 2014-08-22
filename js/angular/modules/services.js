64ed300f66cfb5fe37c433acd4039a902101bf0a
Phillip <phillip.chen@hotmail.com>
2014-08-21 23:51:14 -0700

    cleanup unnecessary success/error handlers

--- js/angular/modules/services.js
+++ js/angular/modules/services.js
@@ -2,50 +2,18 @@ angular.module('jacatucla')
 .factory('jac_services', function($http) {
   var jac_services = {};
 
-  jac_services.get_site_info = $http({method: 'GET', url: 'wp-json/'})
-  .success(function(data, status, headers, config) {
+  jac_services.get_site_info = $http({method: 'GET', url: 'wp-json/'});
 
-  })
-  .error(function(data, status, headers, config) {
-
-  });
-
-  jac_services.get_pages = $http({method: 'GET', url: 'wp-json/pages', params: {'filter[orderby]': 'menu_order', 'filter[order]': 'asc' }})
-  .success(function(data, status, headers, config) {
-
-  })
-  .error(function(data, status, headers, config) {
-
-  });
-
+  jac_services.get_pages = $http({method: 'GET', url: 'wp-json/pages', params: {'filter[orderby]': 'menu_order', 'filter[order]': 'asc' }});
+  
   jac_services.get_posts = function(index) {
-    var get_posts_req = $http({method: 'GET', url: 'wp-json/posts', params: {'page': index}}) 
-    .success(function(data, status, headers, config) {
-
-    })
-    .error(function(data, status, headers, config) {
-
-    });
-    return get_posts_req;
+    return $http({method: 'GET', url: 'wp-json/posts', params: {'page': index}});
   };
 
-  jac_services.get_albums = $http({method: 'GET', url: 'https://picasaweb.google.com/data/feed/api/user/116245231045240410001', params: {'alt': 'json'}})
-  .success(function(data, status, headers, config) {
-    
-  })
-  .error(function(data, status, headers, config) {
-    
-  });
-
+  jac_services.get_albums = $http({method: 'GET', url: 'https://picasaweb.google.com/data/feed/api/user/116245231045240410001', params: {'alt': 'json'}});
+  
   jac_services.get_photos = function(album_id) {
-     var get_photos_req = $http({method: 'GET', url: 'https://picasaweb.google.com/data/feed/api/user/116245231045240410001/albumid/' + album_id, params: {'alt': 'json', 'thumbsize': '160'}}) 
-    .success(function(data, status, headers, config) {
-
-    })
-    .error(function(data, status, headers, config) {
-
-    });
-    return get_photos_req;
+     return $http({method: 'GET', url: 'https://picasaweb.google.com/data/feed/api/user/116245231045240410001/albumid/' + album_id, params: {'alt': 'json', 'thumbsize': '160'}});
   };
 
   return jac_services;
