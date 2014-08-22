58030a8509193956d53ac5848ceb43d99f652c4e
Phillip <phillip.chen@hotmail.com>
2014-08-22 03:05:06 -0700

    basic slider

--- js/angular/controllers/photoset_controller.js
+++ js/angular/controllers/photoset_controller.js
@@ -1,8 +1,26 @@
 angular.module('jacatucla')
-.controller('photoset_controller', function($scope, jac_services, $routeParams) {
+.controller('photoset_controller', function($scope, jac_services, $routeParams, $document) {
 
   var album_id = $routeParams.album_id;
-  
+  $scope.slider = false;
+
+  $scope.show_slider = function() {
+    $scope.slider = true;
+    $document.bind('keydown', function(e) {
+      switch (e.keyCode) {
+        case 27: $scope.hide_slider(); break;
+        case 37: console.log('slide left'); break;
+        case 39: console.log('slide right'); break;
+      }
+    });
+  };
+
+  $scope.hide_slider = function() {
+    $scope.slider = false;
+    $document.unbind('keydown');
+    $scope.$apply();
+  };
+
   jac_services.get_photos(album_id)
   .error(function(data, status, headers, config) {
     console.log('error!');
