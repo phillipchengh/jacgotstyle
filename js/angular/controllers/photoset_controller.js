44850124aa3370a3d24ac27e8970812d9d45bb4f
Phillip <phillip.chen@hotmail.com>
2014-08-23 01:01:15 -0700

    vertically align slider imgs

--- js/angular/controllers/photoset_controller.js
+++ js/angular/controllers/photoset_controller.js
@@ -4,6 +4,11 @@ angular.module('jacatucla')
   var album_id = $routeParams.album_id;
   $scope.slider = false;
 
+  var resize_slider = function() {
+    var slider_height = $(window).height();
+    var slider_width = $(window).width();
+  };
+
   $scope.show_slider = function() {
     $scope.slider = true;
     $document.bind('keydown', function(e) {
@@ -13,11 +18,17 @@ angular.module('jacatucla')
         case 39: console.log('slide right'); break;
       }
     });
+    $(window).resize(resize_slider);
+    resize_slider();
+    $('body').css({'overflow': 'hidden'});
   };
+
 
   $scope.hide_slider = function() {
     $scope.slider = false;
     $document.unbind('keydown');
+    $(window).unbind('resize');
+    $('body').css({'overflow': 'auto'});
     $scope.$apply();
   };
 
--- js/jacatucla.js
+++ js/jacatucla.js
@@ -153,6 +153,11 @@ angular.module('jacatucla')
   var album_id = $routeParams.album_id;
   $scope.slider = false;
 
+  var resize_slider = function() {
+    var slider_height = $(window).height();
+    var slider_width = $(window).width();
+  };
+
   $scope.show_slider = function() {
     $scope.slider = true;
     $document.bind('keydown', function(e) {
@@ -162,11 +167,17 @@ angular.module('jacatucla')
         case 39: console.log('slide right'); break;
       }
     });
+    $(window).resize(resize_slider);
+    resize_slider();
+    $('body').css({'overflow': 'hidden'});
   };
+
 
   $scope.hide_slider = function() {
     $scope.slider = false;
     $document.unbind('keydown');
+    $(window).unbind('resize');
+    $('body').css({'overflow': 'auto'});
     $scope.$apply();
   };
 
