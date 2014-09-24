dbcfeb0bce75dfe50301c8f397d390eb1ac65e81
Phillip <phillip.chen@hotmail.com>
2014-09-23 18:27:40 -0700

    more sidebar tweaks

--- sidebar.php
+++ sidebar.php
@@ -1,29 +1,31 @@
 <div class="sidebar-wrapper" ng-controller="sidebar_controller">
-  <div class="widget-wrapper">
-    <div class="widget-title">
-      Find Us!
+  <div class="sidebar-container">
+    <div class="widget">
+      <div class="widget-title">
+        Find Us!
+      </div>
+      <div class="widget-content">
+        <?php
+          dynamic_sidebar( 'club-info' );
+        ?>
+      </div>
     </div>
-    <div class="widget-content">
+    <div class="widget">
+      <div class="widget-title">
+        Archives
+      </div>
+      <div class="widget-content">
+        <?php
+          wp_get_archives( array(
+            'format' => 'custom',
+            'before' => '<div class="archive-href">',
+            'after' => '</div>'
+          ) );
+        ?>
+      </div>
       <?php
-        dynamic_sidebar( 'club-info' );
+        dynamic_sidebar( 'sidebar-widgets' );
       ?>
     </div>
-  </div>
-  <div class="widget-wrapper">
-    <div class="widget-title">
-      Archives
-    </div>
-    <div class="widget-content">
-      <?php
-        wp_get_archives( array(
-          'format' => 'custom',
-          'before' => '<div>',
-          'after' => '</div>'
-        ) );
-      ?>
-    </div>
-    <?php
-      dynamic_sidebar( 'sidebar-widgets' );
-    ?>
   </div>
 </div>
