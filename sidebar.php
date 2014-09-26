bdd9b9c8cf8cc0e5eb380f12d1a9ec86a8c30284
Phillip <phillip.chen@hotmail.com>
2014-09-25 23:09:37 -0700

    more sidebar styling

--- sidebar.php
+++ sidebar.php
@@ -1,31 +1,7 @@
 <div class="sidebar-wrapper" ng-controller="sidebar_controller">
   <div class="sidebar-container">
-    <div class="widget">
-      <div class="widget-title">
-        Find Us!
-      </div>
-      <div class="widget-content">
-        <?php
-          dynamic_sidebar( 'club-info' );
-        ?>
-      </div>
-    </div>
-    <div class="widget">
-      <div class="widget-title">
-        Archives
-      </div>
-      <div class="widget-content">
-        <?php
-          wp_get_archives( array(
-            'format' => 'custom',
-            'before' => '<div class="archive-href">',
-            'after' => '</div>'
-          ) );
-        ?>
-      </div>
-      <?php
-        dynamic_sidebar( 'sidebar-widgets' );
-      ?>
-    </div>
+    <?php
+      dynamic_sidebar( 'club-info' );
+    ?>
   </div>
 </div>
