35df7c21d4f0fa8f1123c88d988803cc110a1254
Phillip <phillip.chen@hotmail.com>
2014-09-29 13:26:36 -0700

    fresh produce noscript

--- js/jacatucla.js
+++ js/jacatucla.js
@@ -1867,6 +1867,7 @@ angular.module('jacatucla')
   jac_services.get_photos(FP_ANTHOLOGY_ALBUM_ID)
   .success(function(response) {
     $scope.slides = response.feed.entry;
+    angular.element($('.freshproduce-carousel-wrapper')).css('display', 'block');
   })
   .error(function(data, status, headers, config) {
     console.log('error');
--- js/jacatucla/angular/controllers/freshproduce_controller.js
+++ js/jacatucla/angular/controllers/freshproduce_controller.js
@@ -4,6 +4,7 @@ angular.module('jacatucla')
   jac_services.get_photos(FP_ANTHOLOGY_ALBUM_ID)
   .success(function(response) {
     $scope.slides = response.feed.entry;
+    angular.element($('.freshproduce-carousel-wrapper')).css('display', 'block');
   })
   .error(function(data, status, headers, config) {
     console.log('error');
