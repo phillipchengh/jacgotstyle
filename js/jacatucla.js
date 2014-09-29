1f445569de5de9eb3b3991f832cf68c6a004b219
Phillip <phillip.chen@hotmail.com>
2014-09-28 21:29:46 -0700

    fix unused  calls

--- js/jacatucla.js
+++ js/jacatucla.js
@@ -2028,9 +2028,13 @@ angular.module('jacatucla')
 .factory('jac_services', function($http, BASE_PATH) {
   var jac_services = {};
 
-  jac_services.get_site_info = $http({method: 'GET', url: BASE_PATH + '/wp-json/'});
+  jac_services.get_site_info = function() {
+    return $http({method: 'GET', url: BASE_PATH + '/wp-json/'});
+  };
 
-  jac_services.get_pages = $http({method: 'GET', url: BASE_PATH + '/wp-json/pages', params: {'filter[orderby]': 'menu_order', 'filter[order]': 'asc' }});
+  jac_services.get_pages = function() {
+    return $http({method: 'GET', url: BASE_PATH + '/wp-json/pages', params: {'filter[orderby]': 'menu_order', 'filter[order]': 'asc' }});
+  };
   
   jac_services.get_posts = function(index) {
     return $http({method: 'GET', url: BASE_PATH + '/wp-json/posts', params: {'page': index}});
@@ -2044,7 +2048,9 @@ angular.module('jacatucla')
     return $http({method: 'GET', url: BASE_PATH + '/wp-json/posts', params: {'filter[name]': name}});
   };
 
-  jac_services.get_albums = $http({method: 'GET', url: 'https://picasaweb.google.com/data/feed/api/user/116245231045240410001', params: {'alt': 'json'}});
+  jac_services.get_albums = function() {
+    return $http({method: 'GET', url: 'https://picasaweb.google.com/data/feed/api/user/116245231045240410001', params: {'alt': 'json'}});
+  };
   
   jac_services.get_photos = function(album_id) {
      return $http({method: 'GET', url: 'https://picasaweb.google.com/data/feed/api/user/116245231045240410001/albumid/' + album_id, params: {'alt': 'json', 'thumbsize': '160', 'imgmax': '1600'}});
