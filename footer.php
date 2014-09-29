c4b9056ad734b17beb90fa07d355271946dc53e1
Phillip <phillip.chen@hotmail.com>
2014-09-29 16:18:38 -0700

    add footer image and style

--- footer.php
+++ footer.php
@@ -12,7 +12,9 @@ wp_footer();
 <div class="row">
   <div class="col-12">
     <div class="footer-wrapper">
- 	      &copy; 2014 Japanese Animation Club at UCLA
+      <img class="footer-img" src="https://scontent-a-lax.xx.fbcdn.net/hphotos-prn1/t1/1925161_228705723980393_1997456174_n.png"></img>
+      </br>
+ 	    &copy; 2014 Japanese Animation Club at UCLA
     </div>
   </div>
 </div>
