374dbafab06434736758386d93a2f6431189999c
Crystal <asterselene@gmail.com>
2015-11-11 01:55:53 -0800

    sanity test

--- js/jacatucla.js
+++ js/jacatucla.js
@@ -1911,21 +1911,6 @@ angular.module('jacatucla')
   });
 });
 
-angular.module('jacatucla')
-.controller('photos_controller', function($scope, jac_services, BASE_PATH) {
-
-  $scope.BASE_PATH = BASE_PATH;
-
-  jac_services.get_albums()
-  .success(function(response) {
-    $scope.albums = response.feed.entry;
-  })
-  .error(function() {
-    console.log('error');
-  });
-  
-});
-
 var modal_controller = function($scope, $modalInstance, $document, $timeout, photos, start) {
   $scope.slides = photos;
 
@@ -1981,6 +1966,21 @@ angular.module('jacatucla')
     console.log('error!');
   });
 
+});
+
+angular.module('jacatucla')
+.controller('photos_controller', function($scope, jac_services, BASE_PATH) {
+
+  $scope.BASE_PATH = BASE_PATH;
+
+  jac_services.get_albums()
+  .success(function(response) {
+    $scope.albums = response.feed.entry;
+  })
+  .error(function() {
+    console.log('error');
+  });
+  
 });
 
 angular.module('jacatucla')
